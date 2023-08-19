fd47996523c9c51bd3054621a149cf5866f42146
Sunny <rosa7536@naver.com>
2023-08-19 16:18:41 +0900

    [INIT] 프로젝트 생성

---
+++ src/test/java/org/runnect/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.runnect.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0c0d2dcfbcb4ccb6423cca94a303e2db6a17a2d8
Sunny <rosa7536@naver.com>
2023-09-01 17:53:06 +0900

    #8 [Feat] request body validation 처리

--- src/main/java/org/runnect/server/course/controller/CourseController.java
+++ src/main/java/org/runnect/server/course/controller/CourseController.java
@@ -2,6 +2,8 @@ package org.runnect.server.course.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.runnect.server.common.dto.ApiResponseDto;
+import org.runnect.server.common.exception.BadRequestException;
+import org.runnect.server.common.exception.ErrorStatus;
 import org.runnect.server.common.exception.SuccessStatus;
 import org.runnect.server.common.resolver.userId.UserId;
 import org.runnect.server.course.dto.request.CourseCreateRequestDto;
@@ -9,6 +11,7 @@ import org.runnect.server.course.dto.response.CourseCreateResponseDto;
 import org.runnect.server.course.service.CourseService;
 import org.runnect.server.external.aws.S3Service;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,10 +27,16 @@ public class CourseController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponseDto<CourseCreateResponseDto> createCourse(
-            @RequestHeader Long userId,
-            @ModelAttribute @Valid final CourseCreateRequestDto courseCreateRequestDto
+        @RequestHeader Long userId,
+        @ModelAttribute @Valid final CourseCreateRequestDto courseCreateRequestDto,
+        BindingResult bindingResult
     ) {
+        if (bindingResult.hasErrors()) {
+            throw new BadRequestException(ErrorStatus.REQUEST_VALIDATION_EXCEPTION,
+                bindingResult.getFieldError().getField() + " 필드가 입력되지 않았습니다.");
+        }
         String imageUrl = s3Service.uploadImage(courseCreateRequestDto.getImage(), "course");
-        return ApiResponseDto.success(SuccessStatus.CREATE_COURSE_SUCCESS, courseService.createCourse(userId, courseCreateRequestDto, imageUrl));
+        return ApiResponseDto.success(SuccessStatus.CREATE_COURSE_SUCCESS,
+            courseService.createCourse(userId, courseCreateRequestDto, imageUrl));
     }
 }

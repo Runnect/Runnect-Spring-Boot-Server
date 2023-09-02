bc8de66a04027d84c571794afd6e985c911e8475
Sunny <rosa7536@naver.com>
2023-09-03 00:46:25 +0900

    #16 [Feat] ApiResponse 전 버전으로

--- src/main/java/org/runnect/server/common/dto/ApiResponseDto.java
+++ src/main/java/org/runnect/server/common/dto/ApiResponseDto.java
@@ -12,23 +12,24 @@ import org.runnect.server.common.exception.SuccessStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponseDto<T> {
 
-    private final int code;
+    private final int status;
+    private final Boolean success;
     private final String message;
     private T data;
 
     public static ApiResponseDto success(SuccessStatus successStatus) {
-        return new ApiResponseDto<>(successStatus.getHttpStatusCode(), successStatus.getMessage());
+        return new ApiResponseDto<>(successStatus.getHttpStatusCode(), true, successStatus.getMessage());
     }
 
     public static <T> ApiResponseDto<T> success(SuccessStatus successStatus, T data) {
-        return new ApiResponseDto<T>(successStatus.getHttpStatusCode(), successStatus.getMessage(), data);
+        return new ApiResponseDto<T>(successStatus.getHttpStatusCode(), true, successStatus.getMessage(), data);
     }
 
     public static ApiResponseDto error(ErrorStatus errorStatus) {
-        return new ApiResponseDto<>(errorStatus.getHttpStatusCode(), errorStatus.getMessage());
+        return new ApiResponseDto<>(errorStatus.getHttpStatusCode(), false, errorStatus.getMessage());
     }
 
     public static ApiResponseDto error(ErrorStatus errorStatus, String message) {
-        return new ApiResponseDto<>(errorStatus.getHttpStatusCode(), message);
+        return new ApiResponseDto<>(errorStatus.getHttpStatusCode(), false, message);
     }
 }

8a8d4c035ac4716772001948ace6543a2a8cce63
Sunny <rosa7536@naver.com>
2023-09-15 16:55:47 +0900

    #27 [Feat] 마이페이지 조회 API

---
+++ src/main/java/org/runnect/server/user/service/UserService.java
@@ -0,0 +1,30 @@
+package org.runnect.server.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.runnect.server.common.exception.ErrorStatus;
+import org.runnect.server.user.dto.response.MyPageResponseDto;
+import org.runnect.server.user.entity.RunnectUser;
+import org.runnect.server.user.exception.userException.NotFoundUserException;
+import org.runnect.server.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public MyPageResponseDto getMyPage(Long userId) {
+        RunnectUser user = userRepository.findUserByIdWithUserStamps(userId)
+            .orElseThrow(() -> new NotFoundUserException(ErrorStatus.NOT_FOUND_USER_EXCEPTION,
+                ErrorStatus.NOT_FOUND_USER_EXCEPTION.getMessage()));
+        System.out.println("sldkfj");
+        return MyPageResponseDto.of(user, calculateUserLevelPercent(user));
+    }
+
+    private int calculateUserLevelPercent(RunnectUser user) {
+        return (user.getUserStamps().size() % 4) * 25;
+    }
+}

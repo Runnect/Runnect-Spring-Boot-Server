b930b1d8a3a28b7601942157dd9327b1ad87ccaf
Sunny <rosa7536@naver.com>
2023-09-21 13:33:42 +0900

    #43 [Feat] 스크랩 생성시 createdScrap++ & 스탬프 추가 함수

--- src/main/java/org/runnect/server/scrap/service/ScrapService.java
+++ src/main/java/org/runnect/server/scrap/service/ScrapService.java
@@ -1,5 +1,7 @@
 package org.runnect.server.scrap.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.runnect.server.common.exception.ErrorStatus;
 import org.runnect.server.common.exception.NotFoundException;
@@ -13,21 +15,22 @@ import org.runnect.server.scrap.dto.response.UserResponse;
 import org.runnect.server.scrap.entity.Scrap;
 import org.runnect.server.scrap.repository.ScrapRepository;
 import org.runnect.server.user.entity.RunnectUser;
+import org.runnect.server.user.entity.StampType;
 import org.runnect.server.user.exception.userException.NotFoundUserException;
 import org.runnect.server.user.repository.UserRepository;
+import org.runnect.server.user.service.UserStampService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ScrapService {
+
     private final ScrapRepository scrapRepository;
     private final UserRepository userRepository;
     private final PublicCourseRepository publicCourseRepository;
+    private final UserStampService userStampService;
+
     @Transactional
     public void createAndDeleteScrap(Long userId, CreateAndDeleteScrapRequestDto request) {
         Scrap scrap = scrapRepository.findByUserIdAndPublicCourseId(userId, request.getPublicCourseId()).orElse(null);
@@ -43,6 +46,10 @@ public class ScrapService {
                         .publicCourse(publicCourse)
                         .runnectUser(user)
                         .build();
+
+                user.updateCreatedScrap();
+                userStampService.createStampByUser(user, StampType.S);
+
                 scrapRepository.save(newScrap);
             } else {
                 // 기존 스크랩한 내역이 있을 때

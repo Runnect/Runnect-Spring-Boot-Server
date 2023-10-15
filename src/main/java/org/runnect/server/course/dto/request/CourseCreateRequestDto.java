568a63303bfeaca35773341430e802d3fba85956
Sunny <rosa7536@naver.com>
2023-10-15 17:20:27 +0900

    #54 [Fix] CourseCreateRequestDto - image, distance @NotNull로 변경

--- src/main/java/org/runnect/server/course/dto/request/CourseCreateRequestDto.java
+++ src/main/java/org/runnect/server/course/dto/request/CourseCreateRequestDto.java
@@ -1,6 +1,7 @@
 package org.runnect.server.course.dto.request;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,10 +18,10 @@ public class CourseCreateRequestDto {
     @NotBlank(message = "title은 필수 입력 항목입니다.")
     private String title;
 
-    @NotBlank(message = "image는 필수 입력 항목입니다.")
+    @NotNull(message = "image는 필수 입력 항목입니다.")
     private MultipartFile image;
 
-    @NotBlank(message = "distance는 필수 입력 항목입니다.")
+    @NotNull(message = "distance는 필수 입력 항목입니다.")
     private Float distance;
 
     private String departureName;

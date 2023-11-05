e780f3128008109e314d4a9df2e08b55f3fa26d9
Sunny <rosa7536@naver.com>
2023-11-05 15:27:20 +0900

    feat: 컨트롤러 메서드를 작성한다.

--- src/main/java/org/runnect/server/course/controller/CourseController.java
+++ src/main/java/org/runnect/server/course/controller/CourseController.java
@@ -2,14 +2,16 @@ package org.runnect.server.course.controller;
 
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.runnect.server.common.dto.ApiResponseDto;
-import org.runnect.server.common.exception.BadRequestException;
 import org.runnect.server.common.constant.ErrorStatus;
 import org.runnect.server.common.constant.SuccessStatus;
+import org.runnect.server.common.dto.ApiResponseDto;
+import org.runnect.server.common.exception.BadRequestException;
 import org.runnect.server.course.dto.request.CourseCreateRequestDto;
+import org.runnect.server.course.dto.request.DeleteCoursesRequestDto;
 import org.runnect.server.course.dto.request.UpdateCourseRequestDto;
 import org.runnect.server.course.dto.response.CourseCreateResponseDto;
 import org.runnect.server.course.dto.response.CourseGetByUserResponseDto;
+import org.runnect.server.course.dto.response.DeleteCoursesResponseDto;
 import org.runnect.server.course.dto.response.GetCourseDetailResponseDto;
 import org.runnect.server.course.dto.response.UpdateCourseResponseDto;
 import org.runnect.server.course.service.CourseService;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,27 +57,42 @@ public class CourseController {
     @GetMapping("/user")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponseDto<CourseGetByUserResponseDto> getCourseByUser(@RequestHeader Long userId) {
-        return ApiResponseDto.success(SuccessStatus.GET_COURSE_LIST_BY_USER, courseService.getCourseByUser(userId));
+        return ApiResponseDto.success(SuccessStatus.GET_COURSE_LIST_BY_USER,
+            courseService.getCourseByUser(userId));
     }
 
     @GetMapping("/private/user")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<CourseGetByUserResponseDto> getPrivateCourseByUser(@RequestHeader Long userId) {
-        return ApiResponseDto.success(SuccessStatus.GET_COURSE_LIST_BY_USER, courseService.getPrivateCourseByUser(userId));
+    public ApiResponseDto<CourseGetByUserResponseDto> getPrivateCourseByUser(
+        @RequestHeader Long userId) {
+        return ApiResponseDto.success(SuccessStatus.GET_COURSE_LIST_BY_USER,
+            courseService.getPrivateCourseByUser(userId));
     }
 
     @GetMapping("/detail/{courseId}")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<GetCourseDetailResponseDto> getCourseDetail(@RequestHeader Long userId, @PathVariable Long courseId) {
-        return ApiResponseDto.success(SuccessStatus.GET_COURSE_DETAIL_SUCCESS, courseService.getCourseDetail(courseId));
+    public ApiResponseDto<GetCourseDetailResponseDto> getCourseDetail(@RequestHeader Long userId,
+        @PathVariable Long courseId) {
+        return ApiResponseDto.success(SuccessStatus.GET_COURSE_DETAIL_SUCCESS,
+            courseService.getCourseDetail(courseId));
     }
 
     @PatchMapping("/{courseId}")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<UpdateCourseResponseDto> updateCourse(@RequestHeader Long userId, @PathVariable(name = "courseId") Long courseId,
-                                                                @RequestBody @Valid final UpdateCourseRequestDto request) {
-        return ApiResponseDto.success(SuccessStatus.UPDATE_COURSE_SUCCESS, courseService.updateCourse(userId, courseId, request.getTitle()));
+    public ApiResponseDto<UpdateCourseResponseDto> updateCourse(@RequestHeader Long userId,
+        @PathVariable(name = "courseId") Long courseId,
+        @RequestBody @Valid final UpdateCourseRequestDto request) {
+        return ApiResponseDto.success(SuccessStatus.UPDATE_COURSE_SUCCESS,
+            courseService.updateCourse(userId, courseId, request.getTitle()));
     }
 
-
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponseDto<DeleteCoursesResponseDto> deleteCourses(
+        @RequestHeader Long userId,
+        @Valid @RequestBody final DeleteCoursesRequestDto deleteCoursesRequestDto
+    ) {
+        return ApiResponseDto.success(SuccessStatus.DELETE_COURSES_SUCCESS,
+            courseService.deleteCourses(deleteCoursesRequestDto.getCourseIdList(), userId));
+    }
 }

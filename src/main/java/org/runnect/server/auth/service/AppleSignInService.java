fe6d3a9676f9b822622633639f00261a9cbad515
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-06 22:54:00 +0900

    feat: 🎸 애플소셜로그인 테스트완료

--- src/main/java/org/runnect/server/auth/service/AppleSignInService.java
+++ src/main/java/org/runnect/server/auth/service/AppleSignInService.java
@@ -1,8 +1,6 @@
 package org.runnect.server.auth.service;
 
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.RequiredArgsConstructor;
 import org.runnect.server.auth.dto.response.SocialInfoResponseDto;
@@ -10,98 +8,77 @@ import org.runnect.server.common.constant.ErrorStatus;
 import org.runnect.server.common.exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-
-import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jwt.SignedJWT;
-import java.net.URL;
-import com.nimbusds.jose.jwk.RSAKey;
-import java.security.interfaces.RSAPublicKey;
 
-
+import java.text.ParseException;
 
 
 @Service
 @RequiredArgsConstructor
 public class AppleSignInService {
-    @Value("${apple.key_id}")
+    @Value("${apple.key-id}")
     private String APPLE_KEY_ID;
 
-    @Value("${apple.team_id}")
+    @Value("${apple.team-id}")
     private String APPLE_TEAM_ID;
 
-    @Value("${apple.bundle_id}")
+    @Value("${apple.bundle-id}")
     private String APPLE_BUNDLE_ID;
 
-    @Value("${apple.auth_url}")
-    private String APPLE_AUTH_URL;
 
-
+    @Value("${apple.issue-url}")
+    private String APPLE_ISSUE_URL;
 
     public SocialInfoResponseDto getSocialInfo(String idToken) {
-        try{
-            // 클라에서 준 인증토큰이 정말 애플에서 발급받은게 맞는지 확인
 
+
+        // 클라에서 준 인증토큰이 정말 애플에서 발급받은게 맞는지 확인
+
+        try{
             //1. idToken을 parse
             SignedJWT jwt = SignedJWT.parse(idToken);
-            JWSHeader header = jwt.getHeader();
-            String kid = header.getKeyID();
+            JWTClaimsSet claimsSet = jwt.getJWTClaimsSet();
 
+            // 발급처, aud, 시간제한, 이메일 검증
 
-            // apple api를 통해 공개키 3개를 받아온다.
-            URL jwkSetURL = new URL(APPLE_AUTH_URL);
-            JWKSet jwkSet = JWKSet.load(jwkSetURL);
-            //그 중 kid와 동일한 공개키 사용
-            JWK jwk = jwkSet.getKeyByKeyId(kid);
-
-
-            //클라이언트로부터 받은 identity token을 decode한다.
-
-
-            // Convert the JWK to an RSAKey
-            RSAKey rsaKey = (RSAKey) jwk;
-            RSAPublicKey publicKey = rsaKey.toRSAPublicKey();
-            RSASSAVerifier verifier = new RSASSAVerifier(publicKey);
-
-            if (!jwt.verify(verifier)) {
-                new Exception();
+            //만료된경우
+            if (System.currentTimeMillis() < claimsSet.getExpirationTime().getTime()) {
+                throw new UnauthorizedException(ErrorStatus.APPLE_ID_TOKEN_TIME_EXPIRED_EXCEPTION,
+                        ErrorStatus.APPLE_ID_TOKEN_TIME_EXPIRED_EXCEPTION.getMessage());
             }
 
+            // 발급처, aud,이메일 검증 실패시
+            if (!claimsSet.getIssuer().equals(APPLE_ISSUE_URL) ||
+                    !claimsSet.getAudience().get(0).equals(APPLE_BUNDLE_ID) ||
+                    !Boolean.parseBoolean(claimsSet.getStringClaim("email_verified"))) {
+                throw new UnauthorizedException(ErrorStatus.INVALID_APPLE_ID_TOKEN_EXCEPTION,
+                        ErrorStatus.INVALID_APPLE_ID_TOKEN_EXCEPTION.getMessage());
 
-            // Verify claims (issuer, audience, expiration, etc.)
-            JWTClaimsSet claimsSet = jwt.getJWTClaimsSet();
-            System.out.println(claimsSet.getSubject());
-            System.out.println(claimsSet.getStringClaim("email"));
+            }
 
-            return null;
-//        if (!"https://appleid.apple.com".equals(claimsSet.getIssuer()) ||
-//                "your-client-id".equals(claimsSet.getAudience()) ||
-//                System.currentTimeMillis() > claimsSet.getExpirationTime().getTime()) {
-//            return false;
-//        }
-//
-//        // 인증토큰 decoded
-//        String subject = claimsSet.getSubject();
-//        String email = claimsSet.getStringClaim("email");
+            return SocialInfoResponseDto.of(claimsSet.getStringClaim("email"), claimsSet.getSubject());
 
-
-        }catch (Exception e){
-            new UnauthorizedException(ErrorStatus.INVALID_APPLE_ID_TOKEN_EXCEPTION,
+        }catch (ParseException e){
+            throw new UnauthorizedException(ErrorStatus.INVALID_APPLE_ID_TOKEN_EXCEPTION,
                     ErrorStatus.INVALID_APPLE_ID_TOKEN_EXCEPTION.getMessage());
-
-
         }
 
-
-
-        return null;
-
     }
 
-    public static void main(String[] args) {
-        AppleSignInService appleSignInService = new AppleSignInService();
-        appleSignInService.getSocialInfo("");
-    }
+//       id_token 형태 :
+//       {
+//       "aud": 번들아이디,
+//       "sub": 애플 유니크 소셜아이디,
+//       "c_hash":"",
+//       "nonce_supported":true,
+//       "email_verified":"true",
+//       "auth_time":1699270438,
+//       "iss":"https:\/\/appleid.apple.com",
+//       "is_private_email":"true",
+//       "exp":1699356838,
+//       "iat":1699270438,
+//       "email":이메일
+//       }
+
 
 }

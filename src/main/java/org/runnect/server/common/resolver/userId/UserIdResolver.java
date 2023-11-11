97f0b45ac69cb53041987cacf39fa6c5e530512b
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-11 23:40:53 +0900

    fix: 🐛 @UserId에 방문자 모드추가, 방문자모드허용 url에서만 방문자 모드 동작

--- src/main/java/org/runnect/server/common/resolver/userId/UserIdResolver.java
+++ src/main/java/org/runnect/server/common/resolver/userId/UserIdResolver.java
@@ -1,12 +1,15 @@
 package org.runnect.server.common.resolver.userId;
 
+
 import org.runnect.server.common.constant.TokenStatus;
 import org.runnect.server.common.constant.ErrorStatus;
 import lombok.RequiredArgsConstructor;
+import org.runnect.server.common.module.check.TypeChecker;
 import org.runnect.server.user.exception.authException.InvalidAccessTokenException;
 import org.runnect.server.user.exception.authException.NullAccessTokenException;
 import org.runnect.server.user.exception.authException.TimeExpiredAccessTokenException;
 import org.runnect.server.user.exception.userException.NotFoundUserException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -17,12 +20,39 @@ import org.runnect.server.config.jwt.JwtService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @Component
 public class UserIdResolver implements HandlerMethodArgumentResolver {
 
+    @Value("${runnect.visitor-id}")
+    private Long VISITOR_ID;
+    private List<String> VISITOR_POSSIBLE_URLS;
+
+
     private final JwtService jwtService;
+
+    @Value("${runnect.visitor-possible-url}")
+    private void setVISITOR_POSSIBLE_URLS(String VISITOR_POSSIBLE_URL) {
+        this.VISITOR_POSSIBLE_URLS = Stream.of(VISITOR_POSSIBLE_URL.split(","))
+                .map(String::trim).collect(Collectors.toList());
+    }
+
+    private static String removeLastPathSegment(String fullUrI) {
+            String[] segments = fullUrI.split("/");
+            if (segments.length > 0) {
+                //마지막 path variable 찾기
+                String lastSegment = segments[segments.length - 1];
+                if (TypeChecker.isNumeric(lastSegment)) {
+                    return fullUrI.substring(0, fullUrI.lastIndexOf('/'));
+                }
+            }
+            return fullUrI;
+    }
+
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -36,8 +66,17 @@ public class UserIdResolver implements HandlerMethodArgumentResolver {
         String accessToken = request.getHeader("accessToken");
         String refreshToken = request.getHeader("refreshToken");
 
+
+
         if (accessToken == null || refreshToken == null) {
             throw new NullAccessTokenException(ErrorStatus.NULL_ACCESS_TOKEN_EXCEPTION, ErrorStatus.NULL_ACCESS_TOKEN_EXCEPTION.getMessage());
+        }
+
+        if(accessToken.equals("visitor") && refreshToken.equals("visitor")
+                && request.getMethod().equals("GET")
+                && VISITOR_POSSIBLE_URLS.contains(removeLastPathSegment(request.getRequestURI()))){
+            // 방문자모드 허용 api에 대한 요청이 맞는지 검증
+            return VISITOR_ID;
         }
 
 

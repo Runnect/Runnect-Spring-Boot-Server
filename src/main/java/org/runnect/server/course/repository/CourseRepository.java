15f453fad4afaa30a7f4e8cafa74d6faee272d7f
sunny <rosa7536@naver.com>
2023-11-12 16:35:24 +0900

    refactor: 사용하지 않는 함수 제거

--- src/main/java/org/runnect/server/course/repository/CourseRepository.java
+++ src/main/java/org/runnect/server/course/repository/CourseRepository.java
@@ -25,11 +25,9 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     @Query("select c from Course c join fetch c.runnectUser where c.runnectUser.id = :userId and c.isPrivate = true and c.deletedAt = null order by c.createdAt desc")
     List<Course> findCourseByUserIdOnlyPrivate(@Param("userId") Long userId);
 
-    List<Course> findCoursesByRunnectUserAndIsPrivateIsFalse(RunnectUser runnectUser);
     List<Course> findCoursesByRunnectUserAndIsPrivateIsTrue(RunnectUser runnectUser);
 
     List<Course> findCoursesByRunnectUserAndIsPrivateIsFalseAndDeletedAtIsNull(RunnectUser runnectUser);
-    long countByRunnectUser(RunnectUser runnectUser);
 
     Optional<Course> findById(Long courseId);
 

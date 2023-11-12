a5761a4c4a99b2af3512de327e46079d684ebd48
Seonheui Jeon <88873302+funnysunny08@users.noreply.github.com>
2023-11-12 16:43:35 +0900

    [Test] ci/cd 테스트 (#95)
    * feat: 테스트용 빈 코드 추가
    
    * feat: 테스트용 빈 코드 추가

--- src/main/java/org/runnect/server/course/repository/CourseRepository.java
+++ src/main/java/org/runnect/server/course/repository/CourseRepository.java
@@ -35,4 +35,6 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     Optional<Course> findByCourseIdAndUserId(@Param("courseId") Long courseId,  @Param("userId") Long userId);
 
     // DELETE
+
+
 }

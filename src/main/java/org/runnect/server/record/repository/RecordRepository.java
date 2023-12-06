676e6ba74fbe87d9f000f7373a36fc77ccc6a890
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-12-07 00:00:52 +0900

    feat: 🎸 recordRepository에 @Param 추가

--- src/main/java/org/runnect/server/record/repository/RecordRepository.java
+++ src/main/java/org/runnect/server/record/repository/RecordRepository.java
@@ -5,6 +5,7 @@ import org.runnect.server.record.entity.Record;
 import org.runnect.server.user.entity.RunnectUser;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +19,7 @@ public interface RecordRepository extends Repository<Record, Long> {
 
     // publicCourseId가 null일 때도 null값을 반환하기 위해서 LEFT JOIN FETCH 사용
     @Query("SELECT r FROM Record r JOIN FETCH r.course LEFT JOIN FETCH r.publicCourse WHERE r.runnectUser.id = :userId")
-    List<Record> findAllByUserId(Long userId);
+    List<Record> findAllByUserId(@Param("userId")Long userId);
 
     Optional<Record> findById(Long recordId);
 
